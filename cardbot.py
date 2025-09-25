816930b32cbce5590cf598023bbf136966ee0615
Роман <mountgoares@gmail.com>
2025-09-25 20:06:28 +0300

    Обновлён cardbot.py: класс CardGame, команды /start /set /look, улучшено формирование и выдача карт

--- cardbot.py
+++ cardbot.py
@@ -23,20 +23,46 @@ class CardGame:
         return deck
 
     def get_cards(self):
+        empty_slots = 6 - len(self.player_hand)
+        for i in range(empty_slots):
+            card = self.deck.pop()
+            self.player_hand.append(card)
+        return self.player_hand
+    
+    def set_cards(self):
+        self.player_hand = []
         for i in range(6):
             card = self.deck.pop()
             self.player_hand.append(card)
         return self.player_hand
 
+    def look_cards(self):
+        return self.deck
+        
 card_game = CardGame()
 deck = card_game.create_deck()
 
 @dp.message(Command("start"))
-async def start(message: Message):
+async def bot_cards(message: Message):
+        card_game.deck = card_game.create_deck()
         cards = card_game.get_cards()
         cards = "  ".join(cards)
         await message.answer("У вас есть 6 карт, Вы моженте перевыбрать карту. Сыграйте лучшую комбинацию")
         await message.answer(cards)
         
+@dp.message(Command("set"))
+async def bot_set_cards(message: Message):
+        cards = card_game.set_cards()
+        cards = "  ".join(cards)
+        await message.answer("Вы меняете карты. Выши новые карты")
+        await message.answer(cards)
+        
+@dp.message(Command("loo"))
+async def look(message: Message):
+        look_cards = card_game.look_cards()
+        look_card = "  ".join(look_cards)
+        await message.answer(look_card)
+        
 if __name__ == "__main__":
     dp.run_polling(bot)
+    
